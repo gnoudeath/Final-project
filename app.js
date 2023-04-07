6f26be59a401a1096093a35874c14c99e18e1afe
gnoudeath <87683653+gnoudeath@users.noreply.github.com>
2023-04-07 15:22:21 +0700

    view

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require('express');
 const authRouters = require('./routes/authRouters');
+var path = require('path');
 
 const app = express();
 
-// middleware
-app.use(express.static('public'));
-app.use('/bootstrap', express.static(__dirname + 'public/bootstrap'));
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/img', express.static(__dirname + 'public/img'));
-app.use('/js', express.static(__dirname + 'public/js'));
-
 // view engine
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// middleware
+app.use(express.static('public'));
+app.use('/bootstrap', express.static(__dirname + '/public/bootstrap'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/img', express.static(__dirname + '/public/img'));
+app.use('/js', express.static(__dirname + '/public/js'));
 
 // database connection
 const database = process.env.MONGO_DB;
-mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("http://localhost:3000"))
   .catch((err) => console.log(err));
 
@@ -26,3 +29,8 @@ mongoose.connect(database, { useNewUrlParser: true, useUnifiedTopology: true })
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) =>res.render('smoothies'));
 app.use(authRouters);
+
+//Port
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log("Server has started at port " + PORT));
---
+++ public/js/signup.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  //get the values
+  const account = form.account.value;
+  const password = form.password.value;
+
+  console.log(account, password);
+})
