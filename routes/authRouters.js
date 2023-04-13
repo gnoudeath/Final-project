79288fa613e72f9c12c0f423c14055aa869672b1
gnoudeath <87683653+gnoudeath@users.noreply.github.com>
2023-04-13 22:13:10 +0700

    auth router v2

--- routes/authRouters.js
+++ routes/authRouters.js
@@ -16,7 +16,9 @@ router.get('/logout', authController.logout_get);
 router.get('/admin', checkRole('admin'));
 // Section: Customer
 router.get('/home', checkRole('customer'));
-router.get('/about', checkRole('customer'));
+router.get('/about', checkRole('customer'), (req, res) => {
+    res.render('about');
+});
 router.get('/course', checkRole('customer'));
 router.get('/teacher', checkRole('customer'));
 router.get('/blog', checkRole('customer'));
@@ -29,26 +31,26 @@ router.get('/course-learning', checkRole('customer'));
 // router.get('/about', (req, res) => {
 //     res.render('about');
 // })
-// router.get('/course', (req, res) => {
-//     res.render('course');
-// })
-// router.get('/teacher', (req, res) => {
-//     res.render('teacher');
-// })
-// router.get('/blog', (req, res) => {
-//     res.render('blog');
-// })
-// router.get('/contact', (req, res) => {
-//     res.render('contact');
-// })
-// router.get('/single', (req, res) => {
-//     res.render('single');
-// })
-// router.get('/course-detail', (req, res) => {
-//     res.render('course-detail');
-// })
-// router.get('/course-learning', (req, res) => {
-//     res.render('course-learning');
-// })
+router.get('/course', (req, res) => {
+    res.render('course');
+})
+router.get('/teacher', (req, res) => {
+    res.render('teacher');
+})
+router.get('/blog', (req, res) => {
+    res.render('blog');
+})
+router.get('/contact', (req, res) => {
+    res.render('contact');
+})
+router.get('/single', (req, res) => {
+    res.render('single');
+})
+router.get('/course-detail', (req, res) => {
+    res.render('course-detail');
+})
+router.get('/course-learning', (req, res) => {
+    res.render('course-learning');
+})
 
 module.exports = router;
