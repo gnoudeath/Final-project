161c2cbfc8aa2d9306f1df77cdbaa8c186de0d4b
gnoudeath <87683653+gnoudeath@users.noreply.github.com>
2023-04-13 15:50:52 +0700

    submit

--- public/js/course-detail.js
+++ public/js/course-detail.js
@@ -1,12 +1,53 @@
-const navBaiHoc = document.querySelector('.nav-baihoc');
-const tongHopBaiHoc = document.querySelector('.tonghop-baihoc');
-const expandIcon = document.querySelector('.expand-icon');
+const navBaiHocs = document.querySelectorAll('.nav-baihoc');
+const tongHopBaiHocs = document.querySelectorAll('.tonghop-baihoc');
+const expandIcons = document.querySelectorAll('.expand-icon');
+const morongTatCaButton = document.querySelector('.morongvathunho a');
 
-navBaiHoc.addEventListener('click', function() {
-tongHopBaiHoc.classList.toggle('active');
-if (tongHopBaiHoc.classList.contains('active')) {
-    expandIcon.innerHTML = '-';
-} else {
-        expandIcon.innerHTML = '+';
+morongTatCaButton.addEventListener('click', function() {
+    for (let i = 0; i < tongHopBaiHocs.length; i++) {
+        tongHopBaiHocs[i].classList.add('active');
+        expandIcons[i].innerHTML = '-';
     }
+    morongTatCaButton.innerHTML = 'Thu nhỏ tất cả';
 });
+
+for (let i = 0; i < navBaiHocs.length; i++) {
+    navBaiHocs[i].addEventListener('click', function() {
+        tongHopBaiHocs[i].classList.toggle('active');
+        if (tongHopBaiHocs[i].classList.contains('active')) {
+            expandIcons[i].innerHTML = '-';
+        } else {
+            expandIcons[i].innerHTML = '+';
+        }
+    });
+}
+
+const thuNhoTatCaButton = document.createElement('div');
+const linkThuNhoTatCa = document.createElement('a');
+linkThuNhoTatCa.classList.add('btn');
+linkThuNhoTatCa.style.color = 'orangered';
+linkThuNhoTatCa.innerHTML = 'Thu nhỏ tất cả';
+thuNhoTatCaButton.appendChild(linkThuNhoTatCa);
+thuNhoTatCaButton.style.display = 'none';
+
+thuNhoTatCaButton.addEventListener('click', function() {
+    for (let i = 0; i < tongHopBaiHocs.length; i++) {
+        tongHopBaiHocs[i].classList.remove('active');
+        expandIcons[i].innerHTML = '+';
+    }
+    morongTatCaButton.innerHTML = 'Mở rộng tất cả';
+    morongTatCaButton.style.display = 'block';
+    thuNhoTatCaButton.style.display = 'none';
+});
+
+morongTatCaButton.addEventListener('click', function() {
+    for (let i = 0; i < tongHopBaiHocs.length; i++) {
+        tongHopBaiHocs[i].classList.add('active');
+        expandIcons[i].innerHTML = '-';
+    }
+    morongTatCaButton.innerHTML = 'Thu nhỏ tất cả';
+    thuNhoTatCaButton.style.display = 'block';
+    morongTatCaButton.style.display = 'none';
+});
+
+document.querySelector('.morongvathunho').appendChild(thuNhoTatCaButton);
